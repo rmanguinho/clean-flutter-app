414bdb8ebe4ca9f2b5183cb999cb01b02c5e84ff
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-07-23 15:02:48 -0300

    ensure HttpAdapter returns null if post returns 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -88,5 +88,13 @@ void main() {
 
       expect(response, null);
     });
+
+    test('Should return null if post returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
   });
 }
