14f5eecf4a8e3156c2904e2280f72f56fb9ac0e0
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-07-23 14:20:25 -0300

    ensure HttpAdapter calls post with correct URL

--- requirements/http/http_responses.md
+++ requirements/http/http_responses.md
@@ -1,10 +1,11 @@
 # HTTP
 
 > ## Sucesso
-1. Request com verbo http válido (post)
+1. ✅ Request com verbo http válido (post)
 2. Passar nos headers o content type JSON
-3. Ok - 200 e resposta com dados
-4. No content - 204 e resposta sem dados
+3. Chamar request com body correto
+4. Ok - 200 e resposta com dados
+5. No content - 204 e resposta sem dados
 
 > ## Erros
 1. Bad request - 400
---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,34 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    @required String url,
+    @required String method
+  }) async {
+    await client.post(url);
+  }
+}
+
+class ClientSpy extends Mock implements Client {}
+
+void main() {
+  group('post', () {
+    test('Should call post with correct values', () async {
+      final client = ClientSpy();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(url));
+    });
+  });
+}
