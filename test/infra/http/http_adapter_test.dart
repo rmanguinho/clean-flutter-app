fc1c7e90aaa705c149672f9e868d2db8280e1f40
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-07-23 14:24:31 -0300

    ensure HttpAdapter calls post with correct Headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -13,7 +13,11 @@ class HttpAdapter {
     @required String url,
     @required String method
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    await client.post(url, headers: headers);
   }
 }
 
@@ -28,7 +32,13 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        }
+      ));
     });
   });
 }
