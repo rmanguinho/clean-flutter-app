cf70f655c8b02c0b352576056a014a2b586ac26c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-07-23 15:05:10 -0300

    ensure HttpAdapter returns null if post returns 204 with data

--- requirements/http/http_responses.md
+++ requirements/http/http_responses.md
@@ -5,7 +5,7 @@
 2. ✅ Passar nos headers o content type JSON
 3. ✅ Chamar request com body correto
 4. ✅ Ok - 200 e resposta com dados
-5. No content - 204 e resposta sem dados
+5. ✅ No content - 204 e resposta sem dados
 
 > ## Erros
 1. Bad request - 400
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -24,7 +24,11 @@ class HttpAdapter implements HttpClient {
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
   }
 }
 
@@ -91,6 +95,14 @@ void main() {
 
     test('Should return null if post returns 204', () async {
       mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
 
       final response = await sut.request(url: url, method: 'post');
 
