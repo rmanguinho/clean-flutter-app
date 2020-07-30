8262fde4ea64ab4ab3182e8004ef18e3ffac2899
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-07-30 16:52:43 -0300

    ensure StreamLoginPresenter calls Validation with correct email

---
+++ test/presentation/presenters/login/stream_login_presenter_test.dart
@@ -0,0 +1,32 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+abstract class Validation {
+  String validate({@required String field, @required String value});
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+
+  StreamLoginPresenter({@required this.validation});
+
+  void validateEmail(String email) {
+    validation.validate(field: 'email', value: email);
+  }
+}
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  test('Should call Validation with correct email', () {
+    final validation = ValidationSpy();
+    final sut = StreamLoginPresenter(validation: validation);
+    final email = faker.internet.email();
+
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+}
