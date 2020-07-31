073e7414e5870a9875056b58924dc6cc35bb1366
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-07-31 16:04:36 -0300

    ensure StreamLoginPreenter emits error if email is invalid

--- requirements/checklists/login_presenter.md
+++ requirements/checklists/login_presenter.md
@@ -2,7 +2,7 @@
 
 > ## Regras
 1. ✅ Chamar Validation ao alterar o email
-2. Notificar o emailErrorStream com o mesmo erro do Validation, caso retorne erro
+2. ✅ Notificar o emailErrorStream com o mesmo erro do Validation, caso retorne erro
 3. Notificar o emailErrorStream com null, caso o Validation não retorne erro
 4. Não notificar o emailErrorStream se o valor for igual ao último
 5. Notificar o isFormValidStream após alterar o email
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
@@ -7,13 +9,23 @@ abstract class Validation {
   String validate({@required String field, @required String value});
 }
 
+class LoginState {
+  String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({@required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -34,5 +46,14 @@ void main() {
     sut.validateEmail(email);
 
     verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    when(validation.validate(field: anyNamed('field'), value: anyNamed('value')))
+      .thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
