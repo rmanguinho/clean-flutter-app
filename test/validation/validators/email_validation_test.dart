167c5dfd3a89f0e1adfb1c3adfda13dca0cd1fb6
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-03 15:45:34 -0300

    ensure EmailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -8,7 +8,9 @@ class EmailValidation implements FieldValidation {
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -29,5 +31,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('rodrigo.manguinho@gmail.com'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('rodrigo.manguinho'), 'Campo inválido');
   });
 }
