82b898d946383ec230787ef5f34c79afa5dc6087
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-03 15:26:25 -0300

    ensure RequiredFieldValidation returns null if value is not empty

---
+++ test/validation/validators/required_field_validation_test.dart
@@ -0,0 +1,26 @@
+import 'package:test/test.dart';
+
+abstract class FieldValidation {
+  String get field;
+  String validate(String value);
+}
+
+class RequiredFieldValidation implements FieldValidation {
+  final String field;
+
+  RequiredFieldValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main() {
+  test('Should return null if value is not empty', () {
+    final sut = RequiredFieldValidation('any_field');
+
+    final error = sut.validate('any_value');
+
+    expect(error, null);
+  });
+}
