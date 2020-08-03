2c96bd32f9e17bc5a2b8ab167786698a9af255d3
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-03 15:28:46 -0300

    move duplicated code to the setup method

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -16,19 +16,17 @@ class RequiredFieldValidation implements FieldValidation {
 }
 
 void main() {
+  RequiredFieldValidation sut;
+
+  setUp(() {
+    sut = RequiredFieldValidation('any_field');
+  });
+
   test('Should return null if value is not empty', () {
-    final sut = RequiredFieldValidation('any_field');
-
-    final error = sut.validate('any_value');
-
-    expect(error, null);
+    expect(sut.validate('any_value'), null);
   });
 
   test('Should return error if value is empty', () {
-    final sut = RequiredFieldValidation('any_field');
-
-    final error = sut.validate('');
-
-    expect(error, 'Campo obrigatório');
+    expect(sut.validate(''), 'Campo obrigatório');
   });
 }
