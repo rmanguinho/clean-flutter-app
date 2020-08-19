b071c1b93a6fbdd9fd749f44ff730cf5de57836c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-19 12:21:39 -0300

    ensure RemoteAddAccount returns an Account on 200

--- lib/data/usecases/add_account/remote_add_account.dart
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -1,11 +1,13 @@
 import 'package:meta/meta.dart';
 
+import '../../../domain/entities/entities.dart';
 import '../../../domain/helpers/helpers.dart';
 import '../../../domain/usecases/usecases.dart';
 
 import '../../http/http.dart';
+import '../../models/models.dart';
 
-class RemoteAddAccount {
+class RemoteAddAccount implements AddAccount {
   final HttpClient httpClient;
   final String url;
 
@@ -14,10 +16,11 @@ class RemoteAddAccount {
     @required this.url
   });
 
-  Future<void> add(AddAccountParams params) async {
+  Future<AccountEntity> add(AddAccountParams params) async {
     final body = RemoteAddAccountParams.fromDomain(params).toJson();
     try {
-      await httpClient.request(url: url, method: 'post', body: body);
+      final httpResponse = await httpClient.request(url: url, method: 'post', body: body);
+      return RemoteAccountModel.fromJson(httpResponse).toEntity();
     } on HttpError catch(error) {
       throw error == HttpError.forbidden
         ? DomainError.emailInUse
--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -16,8 +16,14 @@ void main() {
   String url;
   AddAccountParams params;
 
+  Map mockValidData() => {'accessToken': faker.guid.guid(), 'name': faker.person.name()};
+
   PostExpectation mockRequest() =>
     when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')));
+
+  void mockHttpData(Map data) {
+    mockRequest().thenAnswer((_) async => data);
+  }
 
   void mockHttpError(HttpError error) {
     mockRequest().thenThrow(error);
@@ -33,6 +39,7 @@ void main() {
       password: faker.internet.password(),
       passwordConfirmation: faker.internet.password()
     );
+    mockHttpData(mockValidData());
   });
 
   test('Should call HttpClient with correct values', () async {
@@ -80,5 +87,14 @@ void main() {
     final future = sut.add(params);
 
     expect(future, throwsA(DomainError.emailInUse));
+  });
+
+  test('Should return an Account if HttpClient returns 200', () async {
+    final validData = mockValidData();
+    mockHttpData(validData);
+
+    final account = await sut.add(params);
+
+    expect(account.token, validData['accessToken']);
   });
 }
