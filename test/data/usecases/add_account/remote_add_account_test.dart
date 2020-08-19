6481258fbf3324c46a36ffd3f274a0d480d9bc92
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-19 12:14:33 -0300

    ensure RemoteAddAccount throws UnexpectedError on 404

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -57,4 +57,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
