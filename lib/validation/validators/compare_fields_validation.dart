672ae305bb77ef48630508f30e8559cdc81700ac
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-25 18:40:33 -0300

    add invalid cases test for CompareFieldsValidation

--- lib/validation/validators/compare_fields_validation.dart
+++ lib/validation/validators/compare_fields_validation.dart
@@ -10,7 +10,10 @@ class CompareFieldsValidation implements FieldValidation {
 
   CompareFieldsValidation({@required this.field, @required this.fieldToCompare});
 
-  ValidationError validate(Map input) {
-    return input[field] == input[fieldToCompare] ? null : ValidationError.invalidField;
-  }
+  ValidationError validate(Map input) =>
+    input[field] != null &&
+    input[fieldToCompare] != null &&
+    input[field] != input[fieldToCompare] ?
+      ValidationError.invalidField
+      : null;
 }
--- test/validation/validators/compare_fields_validation_test.dart
+++ test/validation/validators/compare_fields_validation_test.dart
@@ -11,6 +11,12 @@ void main() {
     sut = CompareFieldsValidation(field: 'any_field', fieldToCompare: 'other_field');
   });
 
+  test('Should return null on invalid cases', () {
+    expect(sut.validate({'any_field': 'any_value'}), null);
+    expect(sut.validate({'other_field': 'any_value'}), null);
+    expect(sut.validate({}), null);
+  });
+
   test('Should return error if values are not equal', () {
     final formData = {'any_field': 'any_value', 'other_field': 'other_value'};
     expect(sut.validate(formData), ValidationError.invalidField);
