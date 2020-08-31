8dd8e2a212cc79b478cd16e6b61410c91fdfdd54
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-31 17:03:56 -0300

    ensure HttpAdapter throws ServerError if HttpClient throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -161,6 +161,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -249,6 +253,14 @@ void main() {
 
     test('Should return ServerError if get returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if get throws', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'get');
 
