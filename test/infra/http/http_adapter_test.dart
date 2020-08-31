6ce3fec5e84e6606b72bf92c0c8b2970fe80255a
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-31 17:02:24 -0300

    ensure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -246,5 +246,13 @@ void main() {
 
       expect(future, throwsA(HttpError.notFound));
     });
+
+    test('Should return ServerError if get returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
