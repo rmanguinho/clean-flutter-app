6bf6be6fd5ab83b0da498a6530e2196291028650
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-31 16:58:37 -0300

    ensure HttpAdapter returns null on 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -190,5 +190,21 @@ void main() {
 
       expect(response, null);
     });
+
+    test('Should return null if get returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
+    });
+
+    test('Should return null if get returns 204 with data', () async {
+      mockResponse(204);
+
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
+    });
   });
 }
