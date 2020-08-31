702df572a53cdfb86251e647efc12cfcfc52acc8
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-08-31 16:57:38 -0300

    ensure HttpAdapter returns data on 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -176,5 +176,19 @@ void main() {
         }
       ));
     });
+
+    test('Should return data if get returns 200', () async {
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if get returns 200 with no data', () async {
+      mockResponse(200, body: '');
+
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
+    });
   });
 }
