05ee256a40565f8042748e2ee06a9dc118bc1455
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-02 14:43:30 -0300

    remove unused tests

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -65,28 +65,6 @@ void main() {
     closeStreams();
   });
 
-  testWidgets('Should load with correct initial state', (WidgetTester tester) async {
-    await loadPage(tester);
-
-    final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
-    expect(
-      emailTextChildren,
-      findsOneWidget,
-      reason: 'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text'
-    );
-
-    final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
-    expect(
-      passwordTextChildren,
-      findsOneWidget,
-      reason: 'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the label text'
-    );
-
-    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
-    expect(button.onPressed, null);
-    expect(find.byType(CircularProgressIndicator), findsNothing);
-  });
-
   testWidgets('Should call validate with correct values', (WidgetTester tester) async {
     await loadPage(tester);
 
