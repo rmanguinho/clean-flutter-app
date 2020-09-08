135b19bcb31fb282985d391c0605487c91632d41
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-08 15:08:43 -0300

    ensure Decorator returns same result as decoratee

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -6,7 +6,7 @@ import 'package:test/test.dart';
 import 'package:ForDev/data/cache/cache.dart';
 import 'package:ForDev/data/http/http.dart';
 
-class AuthorizeHttpClientDecorator {
+class AuthorizeHttpClientDecorator implements HttpClient {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
   final HttpClient decoratee;
 
@@ -15,7 +15,7 @@ class AuthorizeHttpClientDecorator {
     @required this.decoratee,
   });
 
-  Future<void> request({
+  Future<dynamic> request({
     @required String url,
     @required String method,
     Map body,
@@ -23,7 +23,7 @@ class AuthorizeHttpClientDecorator {
   }) async {
     final token = await fetchSecureCacheStorage.fetchSecure('token');
     final authorizedHeaders = headers ?? {}..addAll({'x-access-token': token});
-    await decoratee.request(url: url, method: method, body: body, headers: authorizedHeaders);
+    return await decoratee.request(url: url, method: method, body: body, headers: authorizedHeaders);
   }
 }
 
@@ -38,10 +38,21 @@ void main() {
   String method;
   Map body;
   String token;
+  String httpResponse;
 
   void mockToken() {
     token = faker.guid.guid();
     when(fetchSecureCacheStorage.fetchSecure(any)).thenAnswer((_) async => token);
+  }
+
+  void mockHttpResponse() {
+    httpResponse = faker.randomGenerator.string(50);
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+      headers: anyNamed('headers'),
+    )).thenAnswer((_) async => httpResponse);
   }
 
   setUp(() {
@@ -55,6 +66,7 @@ void main() {
     method = faker.randomGenerator.string(10);
     body = {'any_key': 'any_value'};
     mockToken();
+    mockHttpResponse();
   });
 
   test('Should call FetchSecureCacheStorage with correct key', () async {
@@ -74,5 +86,11 @@ void main() {
       body: body,
       headers: {'x-access-token': token, 'any_header': 'any_value'}
     )).called(1);
+  });
+
+  test('Should return same result as decoratee', () async {
+    final response = await sut.request(url: url, method: method, body: body);
+
+    expect(response, httpResponse);
   });
 }
