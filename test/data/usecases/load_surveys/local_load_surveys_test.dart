258d0efba44a33ac60ea3d69f4329072a72ec3d9
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-15 09:18:31 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache is isvalid

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -13,10 +13,14 @@ class LocalLoadSurveys {
 
   Future<List<SurveyEntity>> load() async {
     final data = await fetchCacheStorage.fetch('surveys');
-    if (data?.isEmpty != false) {
+    try {
+      if (data?.isEmpty != false) {
+        throw Exception();
+      }
+      return data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
+    } catch(error) {
       throw DomainError.unexpected;
     }
-    return data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
   }
 }
 
@@ -81,6 +85,19 @@ void main() {
 
   test('Should throw UnexpectedError if cache is null', () async {
     mockFetch(null);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if cache is isvalid', () async {
+    mockFetch([{
+      'id': faker.guid.guid(),
+      'question': faker.randomGenerator.string(10),
+      'date': 'invalid date',
+      'didAnswer': 'false',
+    }]);
 
     final future = sut.load();
 
