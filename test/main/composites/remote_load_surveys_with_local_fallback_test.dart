cff432759a3e7a725015e03a238a9d09c62688f6
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-16 15:14:32 -0300

    move SUT creation to the setup method

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -19,12 +19,17 @@ class RemoteLoadSurveysWithLocalFallback {
 class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
 
 void main() {
-  test('Should call remote load', () async {
-    final remote = RemoteLoadSurveysSpy();
-    final sut = RemoteLoadSurveysWithLocalFallback(
+  RemoteLoadSurveysWithLocalFallback sut;
+  RemoteLoadSurveysSpy remote;
+
+  setUp(() {
+    remote = RemoteLoadSurveysSpy();
+    sut = RemoteLoadSurveysWithLocalFallback(
       remote: remote
     );
+  });
 
+  test('Should call remote load', () async {
     await sut.load();
 
     verify(remote.load()).called(1);
