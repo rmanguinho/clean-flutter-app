91822591fc0313a0123bd4cd6de1afcf087e18e1
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-16 16:25:18 -0300

    remove code duplication

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -17,11 +17,15 @@ class HttpAdapter implements HttpClient {
     });
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
+    Future<Response> futureResponse;
     try {
       if (method == 'post') {
-        response = await client.post(url, headers: defaultHeaders, body: jsonBody).timeout(Duration(seconds: 10));
+        futureResponse = client.post(url, headers: defaultHeaders, body: jsonBody);
       } else if (method == 'get') {
-        response = await client.get(url, headers: defaultHeaders).timeout(Duration(seconds: 10));
+        futureResponse = client.get(url, headers: defaultHeaders);
+      }
+      if (futureResponse != null) {
+        response = await futureResponse.timeout(Duration(seconds: 10));
       }
     } catch(error) {
       throw HttpError.serverError;
