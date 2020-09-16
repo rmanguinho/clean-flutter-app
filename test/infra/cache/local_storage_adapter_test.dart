103f27b5a957e3d30e1132fd1fcb5fe668783703
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-16 10:12:13 -0300

    ensure LocalStorageAdapter calls deleteItem with correct value

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -10,6 +10,7 @@ class LocalStorageAdapter {
   LocalStorageAdapter({@required this.localStorage});
 
   Future<void> save({@required String key, @required dynamic value}) async {
+    await localStorage.deleteItem(key);
     await localStorage.setItem(key, value);
   }
 }
@@ -32,6 +33,7 @@ void main() {
   test('Should call localStorage with correct values', () async {
     await sut.save(key: key, value: value);
 
+    verify(localStorage.deleteItem(key)).called(1);
     verify(localStorage.setItem(key, value)).called(1);
   });
 }
