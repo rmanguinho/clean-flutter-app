baa1263c4b07929ba4cd8c4fa9fc705869a40124
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-16 10:48:37 -0300

    ensure LocalStorageAdapter throws if getItem throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -68,7 +68,11 @@ void main() {
   group('fetch', () {
     String result;
 
-    void mockFetch() => when(localStorage.getItem(any)).thenAnswer((_) async => result);
+    PostExpectation mockFetchCall() => when(localStorage.getItem(any));
+
+    void mockFetch() => mockFetchCall().thenAnswer((_) async => result);
+
+    void mockFetchError() => mockFetchCall().thenThrow(Exception());
 
     setUp(() {
       mockFetch();
@@ -84,6 +88,14 @@ void main() {
       final data = await sut.fetch(key);
 
       expect(data, result);
+    });
+
+    test('Should throw if getItem throws', () async {
+      mockFetchError();
+
+      final future = sut.fetch(key);
+
+      expect(future, throwsA(TypeMatcher<Exception>()));
     });
   });
 }
