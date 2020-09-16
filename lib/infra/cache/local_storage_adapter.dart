dd7784cd05b92865df3ac74ef33105500ce663e1
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-09-16 10:35:33 -0300

    ensure LocalStorageAdapter deletes cache with correct key

--- lib/infra/cache/local_storage_adapter.dart
+++ lib/infra/cache/local_storage_adapter.dart
@@ -10,4 +10,8 @@ class LocalStorageAdapter {
     await localStorage.deleteItem(key);
     await localStorage.setItem(key, value);
   }
+
+  Future<void> delete(String key) async {
+    await localStorage.deleteItem(key);
+  }
 }
--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -48,4 +48,12 @@ void main() {
       expect(future, throwsA(TypeMatcher<Exception>()));
     });
   });
+
+  group('delete', () {
+    test('Should call localStorage with correct values', () async {
+      await sut.delete(key);
+
+      verify(localStorage.deleteItem(key)).called(1);
+    });
+  });
 }
