bc94f7d25028d9582b5a611e2c2c77a8f5c7d8ee
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-10-21 17:32:15 -0300

    ensure RemoteSaveSurveyResult throws DomainError on success but with invalid data

--- test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -83,6 +83,14 @@ void main() {
     ));
   });
 
+  test('Should throw UnexpectedError if HttpClient returns 200 with invalid data', () async {
+    mockHttpData({'invalid_key': 'invalid_value'});
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
   test('Should throw UnexpectedError if HttpClient returns 404', () async {
     mockHttpError(HttpError.notFound);
 
