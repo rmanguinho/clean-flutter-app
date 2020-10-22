39de38895a8fc144670da30a07b19b782f713fa7
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-10-22 14:57:02 -0300

    remove duplication

--- lib/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/presentation/presenters/getx_survey_result_presenter.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:meta/meta.dart';
 
+import '../../domain/entities/entities.dart';
 import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
 import '../../ui/helpers/helpers.dart';
@@ -22,34 +23,17 @@ class GetxSurveyResultPresenter extends GetxController with LoadingManager, Sess
   });
 
   Future<void> loadData() async {
-    try {
-      isLoading = true;
-      final surveyResult = await loadSurveyResult.loadBySurvey(surveyId: surveyId);
-      _surveyResult.value = SurveyResultViewModel(
-        surveyId: surveyResult.surveyId,
-        question: surveyResult.question,
-        answers: surveyResult.answers.map((answer) => SurveyAnswerViewModel(
-          image: answer.image,
-          answer: answer.answer,
-          percent: '${answer.percent}%',
-          isCurrentAnswer: answer.isCurrentAnswer
-        )).toList()
-      );
-    } on DomainError catch(error) {
-      if (error == DomainError.accessDenied) {
-        isSessionExpired = true;
-      } else {
-        _surveyResult.subject.addError(UIError.unexpected.description);
-      }
-    } finally {
-      isLoading = false;
-    }
+    showResultOnAction(() => loadSurveyResult.loadBySurvey(surveyId: surveyId));
   }
 
   Future<void> save({@required String answer}) async {
+    showResultOnAction(() => saveSurveyResult.save(answer: answer));
+  }
+
+  Future<void> showResultOnAction(Future<SurveyResultEntity> action()) async {
     try {
       isLoading = true;
-      final surveyResult = await saveSurveyResult.save(answer: answer);
+      final surveyResult = await action();
       _surveyResult.value = SurveyResultViewModel(
         surveyId: surveyResult.surveyId,
         question: surveyResult.question,
