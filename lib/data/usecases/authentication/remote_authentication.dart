21b215d3710a59bbe9c1dbcda48ae1b9646d4caa
Lucas Fidelis <lucasfidelis97@gmail.com>
2022-06-10 07:51:01 -0300

    feat: ensure RemoteAuthentication throw UnexpectedError if RemoteAccountModel throws when HttpClient returns 204

--- lib/data/usecases/authentication/remote_authentication.dart
+++ lib/data/usecases/authentication/remote_authentication.dart
@@ -8,20 +8,17 @@ class RemoteAuthentication implements Authentication {
   final HttpClient httpClient;
   final String url;
 
-  RemoteAuthentication({
-    required this.httpClient,
-    required this.url
-  });
+  RemoteAuthentication({required this.httpClient, required this.url});
 
   Future<AccountEntity> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     try {
       final httpResponse = await httpClient.request(url: url, method: 'post', body: body);
       return RemoteAccountModel.fromJson(httpResponse).toEntity();
-    } on HttpError catch(error) {
-      throw error == HttpError.unauthorized
-        ? DomainError.invalidCredentials
-        : DomainError.unexpected;
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized ? DomainError.invalidCredentials : DomainError.unexpected;
+    } catch (error) {
+      throw DomainError.unexpected;
     }
   }
 }
@@ -30,13 +27,9 @@ class RemoteAuthenticationParams {
   final String email;
   final String password;
 
-  RemoteAuthenticationParams({
-    required this.email,
-    required this.password
-  });
+  RemoteAuthenticationParams({required this.email, required this.password});
 
-  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) => 
-    RemoteAuthenticationParams(email: params.email, password: params.secret);
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) => RemoteAuthenticationParams(email: params.email, password: params.secret);
 
   Map toJson() => {'email': email, 'password': password};
 }
